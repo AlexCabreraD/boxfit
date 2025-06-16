afde5bed73a7fd272a85312d8c2ebcc18c242322
AlexCabreraD <109772528+AlexCabreraD@users.noreply.github.com>
2025-06-16 15:14:43 -0600

    remove optimizeCss

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: true,
-  },
-
   images: {
     domains: ["boxfit-utah.com"],
     formats: ["image/webp", "image/avif"],
